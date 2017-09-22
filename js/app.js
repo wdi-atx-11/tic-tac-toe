3fa9a86ea5f6af3efaeb45cb46b64163c9a489cb
Peter Kong <bansuki@gmail.com>
2017-09-22 10:34:29 -0500

    generating game board with minimal styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
-// wait for the DOM to finish loading
-$(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+let gameCells = createGameBoard();
 
-});
+// loop through turns
+let gameWon = false;
+let isXTurn = true;
+// while(!gameWon) {
+//   if(gameWon) {
+//     // do stuff
+//     break;
+//   }
+// }
+
+function createGameBoard() {
+  let table = document.querySelector('.game-board');
+
+  for(let i = 0; i < 3; i++) {
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+    for(let j = 0; j < 3; j++) {
+      let td = document.createElement('td');
+      tr.appendChild(td);
+      td.innerText = 'x';
+      td.setAttribute('class', 'game-cell');
+    }
+  }
+  return [...table.querySelectorAll('.game-cell')];
+}
+
+function addCellListeners(table) {
+
+}
+
+function isGameWon(table) {
+
+  return false;
+}
