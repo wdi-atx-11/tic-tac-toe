ce25084d44a3ab3f1a57407db78813acdaa86774
Peter Kong <bansuki@gmail.com>
2017-09-22 10:56:08 -0500

    added reset

--- js/app.js
+++ js/app.js
@@ -3,34 +3,51 @@ let gameCells = createGameBoard();
 // loop through turns
 let gameWon = false;
 let isXTurn = true;
-// while(!gameWon) {
-//   if(gameWon) {
-//     // do stuff
-//     break;
-//   }
-// }
 
+// make reset button work
+document.querySelector('button').addEventListener('click', resetBoard);
+
+
+
+// helper functions
 function createGameBoard() {
   let table = document.querySelector('.game-board');
+  let tbody = document.createElement('tbody');
+  table.appendChild(tbody);
 
   for(let i = 0; i < 3; i++) {
     let tr = document.createElement('tr');
-    table.appendChild(tr);
+    tbody.appendChild(tr);
     for(let j = 0; j < 3; j++) {
       let td = document.createElement('td');
       tr.appendChild(td);
-      td.innerText = 'x';
+      td.innerText = '';
       td.setAttribute('class', 'game-cell');
     }
   }
   return [...table.querySelectorAll('.game-cell')];
 }
 
-function addCellListeners(table) {
-
+function addCellListeners(cells) {
+  cells.forEach(cell => {
+    cell.addEventListener('click', playTurn);
+  });
 }
 
 function isGameWon(table) {
 
   return false;
+}
+
+
+// callbacks
+function playTurn(e) {
+  let token = isXTurn ? 'x' : 'o';
+  this.innerText = token;
+}
+
+function resetBoard() {
+  let table = document.querySelector('.game-board');
+  table.removeChild(table.children[0]);
+  gameCells = createGameBoard();
 }
