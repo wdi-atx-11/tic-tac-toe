d97ff1594f5b23772df4c942eb3cef664eb44564
Peter Kong <bansuki@gmail.com>
2017-09-22 11:33:52 -0500

    made turns write-only until reset; added win conditions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-let gameCells = createGameBoard();
+let gameCells = [];
+createGameBoard();
 
 // loop through turns
 let isXTurn = true;
@@ -15,8 +16,10 @@ function createGameBoard() {
   for(let i = 0; i < 3; i++) {
     let tr = document.createElement('tr');
     tbody.appendChild(tr);
+    gameCells.push([]);
     for(let j = 0; j < 3; j++) {
       let td = document.createElement('td');
+      gameCells[gameCells.length-1].push(td);
       tr.appendChild(td);
       td.innerText = '';
       td.setAttribute('class', 'game-cell');
@@ -24,19 +27,46 @@ function createGameBoard() {
       td.addEventListener('click', playTurn);
     }
   }
-  return [...table.querySelectorAll('.game-cell')];
 }
 
-function isGameWon(cell) {
-
+function isGameWon() {
+  [
+    [0,0, 1,0, 2,0]
+    [0,0, 0,1, 1,2],
+    [0,0, 1,1, 2,2],
+    [1,0, 1,1, 1,2],
+    [2,0, 2,1, 2,2],
+    [2,0, 1,1, 0,2],
+    [0,1, 1,1, 2,1],
+    [0,2, 1,2, 2,2]
+  ].forEach(con => {
+    if(checkWinCondition(con)) {
+      return true;
+    }
+  });
   return false;
 }
 
+function checkWinCondition(ar) {
+  for(let i = 0; i < ar.length; i+=2) {
+    let coord = ar.slice(i,i+1);
+    if(gameCells[coord[0]*coord[1]] !== (isXTurn ? 'x' : 'o')) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // callbacks
 function playTurn(e) {
   let token = isXTurn ? 'x' : 'o';
+
   this.innerText = token;
+  this.removeEventListener('click', playTurn);
+  if(isGameWon()) {
+    alert(`${token} wins!`);
+  }
+
   isXTurn = !isXTurn;
 }
 
